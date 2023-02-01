ad3c74cf7987f893e0e968b394ccf1f0dcd7cd31
james4-daf <d.james@elsevier.com>
2023-02-01 12:45:50 +0000

    refactored it 9

--- src/components/ClickablePicture.jsx
+++ src/components/ClickablePicture.jsx
@@ -2,23 +2,16 @@ import React, { useState } from "react";
 
 function ClickablePicture(props) {
   const { image, imgClicked } = props;
-
   const [image2, setImage] = useState(false);
-
-  function handleClick() {
-    if (!image2) {
-      setImage(true);
-    } else {
-      setImage(false);
-    }
-  }
 
   return (
     <div>
       <img
         alt=""
         src={!image2 ? image : imgClicked}
-        onClick={handleClick}
+        onClick={() => {
+          setImage((prevValue) => !prevValue);
+        }}
         style={{ height: 100, width: 100 }}
       ></img>
     </div>
